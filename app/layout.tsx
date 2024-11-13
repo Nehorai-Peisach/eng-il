7638e2e32e83b6baed3bf55c6a1044dbe7ca4fce
Neho <87582902+Nehorai-Peisach@users.noreply.github.com>
2024-11-14 00:43:09 +0200

    og

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,18 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "ENG-IL",
   description: "Learn English Easy",
+  openGraph: {
+    images: [
+      {
+        url: "/og.png", // This points to your existing image
+        width: 1200,
+        height: 630,
+        alt: "ENG-IL - Learn English Easy",
+      },
+    ],
+  },
 };
+
 
 export default function RootLayout({
   children,
@@ -19,6 +30,7 @@ export default function RootLayout({
       <meta name="theme-color" content="#021f25" />
       <meta name="msapplication-navbutton-color" content="#021f25" />
       <meta name="apple-mobile-web-app-status-bar-style" content="#021f25" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"/>
       <body className={inter.className}>{children}</body>
     </html>
   );
